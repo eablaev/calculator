e3b1394b85812c49223fb322ab860387aa1cbd93
shmerv <eaudioworks@gmail.com>
2022-10-06 09:03:31 -0700

    add operate func

--- calculator.js
+++ calculator.js
@@ -1,24 +1,34 @@
 function add(a,b){
     return a+b;
 }
-
 function subtract(a,b){
     return a-b;
 }
-
 function multiply(a,b){
     return a*b;
 }
-
 function divide(a,b){
     return a/b;
 }
 
+function operate(op,a,b){
+    if(op === "+"){
+        return add(a,b);
+    }
+    else if(op === "-"){
+        return subtract(a,b);
+    }
+    else if(op === "*"){
+        return multiply(a,b);
+    }
+    else if(op === "/"){
+        return divide(a,b);
+    }
+}
 
 
 
 
 
 
-
-console.log(divide(10,5))
+console.log(operate('+',10,5))
